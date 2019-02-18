97d64a8122251e9d22d36930e76d328538f462b2
mzh <939030481@qq.com>
2019-02-18 09:47:44 +0800

    考试

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp');
+var minCss = require('gulp-clean-css');
+var sass = require('gulp-sass');
+var server = require('gulp-webserver');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var url = require('url');
+var path = require('path');
+var fs = require('fs');
+
+//起服务
+gulp.task('server', function () {
+    gulp.src('src')
+        .pipe(server({
+            port: 8080,
+            open: true,
+            livereload: true
+        }))
+})
+
+//编译sass
+gulp.task('sass', function () {
+    return gulp.src('./src/sass/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./src/css'))
+})
+//压缩css
+gulp.task('minCss', function () {
+    return gulp.src('./src/css/**/*.css')
+        .pipe(sass())
+        .pipe(minCss())
+        .pipe(gulp.dest('./dist/css'))
+})
+//监听css
+gulp.task('watch', function () {
+    return gulp.watch('./src/sass/**/*.scss', gulp.series('sass'))
+})
+//编译合并js
+gulp.task('minJs', function () {
+    return gulp.src("./src/js/**/*.js")
+        .pipe(uglify())
+        .pipe(concat('build.js'))
+        .pipe(gulp.dest('./dist/js'))
+
+})
+
+gulp.task('default', gulp.series('server', 'minJs', 'sass', 'watch'));
+gulp.task('build', gulp.parallel('minJs', 'minCss'));
